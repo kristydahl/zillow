fd12fef01ee5658d134bbfd841a7991ef4a0a062
kristydahl <kdahl@alum.mit.edu>
2017-09-21 11:38:39 -0700

    zillow_exploration working. This code joins fields of interest from zillow's data tables, weeds out duplicates, selects only coastal properties, and geocodes properties that lack a lat/long.
    chronic_inundation_property_analysis is just pseudocode right now.

---
+++ .idea/chronic_inundation_property_analysis.py
@@ -0,0 +1,38 @@
+#import arcpy
+
+# Clip leveed areas out of properties files
+# def remove_properties_in_leveed_areas(state_numbers):
+    # leveed_areas = [shapefile]
+    # for state in state_numbers:
+        # all_properties = read in shapefile of properties
+        # outfile = coastal_properties_state_{0}_nonleveed .format(state)
+        # arcpy.Erase_management(all_properties, leveed_areas, outfile)
+
+
+# ID CI properties for each state, year, and projection
+# def identify_ci_properties(state_numbers, years, projection):
+    # for state in state_numbers:
+        # properties = read in data file (shapefile of properties-nonleveed)
+
+        # for year in years:
+            # inundated_area = shapefile (pass in state, year, projection}
+            # outfile = ci_properties_state_{0}_{1}_{2} .format(state, year, projection)
+            # ci_properties = arcpy.SelectByLocation_managment(properties, inundated_area, "WITHIN") # check parameters here
+            # ci_properties.save(outfile)
+
+# Output state statistics for year, projection, number of CI properties, total value of CI properties
+    # Output as shapefile
+    # Output as CSV
+
+# Split shapefiles of CI properties by county subdivision boundaries
+
+# Output year, projection, number of CI properties, total value of CI properties per county subdivision
+    # Output as shapefile
+    # Output as CSV
+
+
+# Analyze other parameters (year built, foundation type, loan type, loan due date)
+    # Of the CI properties, how many will be CI before mortgage is paid off?
+    # Of the CI properties, are there patterns to age of building? I.e. are there more old houses getting CI first?
+    # Square footage
+    # Property taxes
